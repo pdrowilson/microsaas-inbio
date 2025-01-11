b135446121b71ae52e28e810bb5f2390571913dc
Pedro Wilson <pdro.wilson@gmail.com>
2025-01-11 11:04:41 -0300

    add Hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 import Image from "next/image";
+import Hero from "./Components/landing-page/Hero";
 
 export default function Home() {
-  return <div className="bg-background-primary">Hello World</div>;
+  return (
+    <div className="max-w-7xl mx-auto border">
+      <Hero />
+      {/* <Header />
+    <VideoExplanation />
+    <Pricing />
+    <FAQ /> */}
+    </div>
+  );
 }
