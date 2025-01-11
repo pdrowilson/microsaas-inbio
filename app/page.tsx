ee828b2f174a0a70b124516be8d2aab043f4488c
Pedro Wilson <pdro.wilson@gmail.com>
2025-01-11 12:05:21 -0300

    Add Header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
-import Image from "next/image";
 import Hero from "./components/landing-page/Hero";
+import Header from "./components/landing-page/Header";
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto">
+      <Header />
       <Hero />
-      {/* <Header />
-    <VideoExplanation />
-    <Pricing />
-    <FAQ /> */}
+      {/* 
+      <VideoExplanation />
+      <Pricing />
+      <FAQ /> */}
     </div>
   );
 }
