a7c15128ded3e953f5886556de4a9f4eeaece52e
Pedro Wilson <pdro.wilson@gmail.com>
2025-01-11 11:50:49 -0300

    Add Hero component

--- app/components/UI/Button.tsx
+++ app/components/UI/Button.tsx
@@ -12,7 +12,7 @@ export default function Button({
     <button
       {...props}
       className={cn(
-        "p3 text-white rounded-xl font-bold whitespace-nowrap hover:opacity-95 disabled:opacity-70",
+        "p-3 text-white rounded-xl font-bold whitespace-nowrap hover:opacity-95 disabled:opacity-70",
         variant === "primary" && "bg-accent-purple",
         variant === "secondary" && "bg-background-tertiary",
         variant === "ghost" && "border-border-primary",
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "./components/landing-page/Hero";
 
 export default function Home() {
   return (
-    <div className="max-w-7xl mx-auto border">
+    <div className="max-w-7xl mx-auto">
       <Hero />
       {/* <Header />
     <VideoExplanation />
