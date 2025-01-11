0b6e45bed905017d71b6990d9390e55029bee749
Pedro Wilson <pdro.wilson@gmail.com>
2025-01-11 12:10:07 -0300

    Add Video and Pricing components

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import Hero from "./components/landing-page/Hero";
 import Header from "./components/landing-page/Header";
+import VideoExplanation from "./components/landing-page/VideoExplanation";
+import Pricing from "./components/landing-page/Pricing";
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto">
       <Header />
       <Hero />
-      {/* 
       <VideoExplanation />
       <Pricing />
+      {/* 
       <FAQ /> */}
     </div>
   );
